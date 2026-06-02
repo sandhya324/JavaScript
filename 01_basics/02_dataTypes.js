540e0fa2177b28ec28f4918a165d73a4d2837b89
SanDhya <141916657+sandhya324@users.noreply.github.com>
2026-06-02 15:35:02 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as never version
+
+// alert(3 + 3) //we are using nodejs , not browser
+ 
+console.log(3
+     + 3)   //code readability should be high 
+
+console.log("Sandhya")
+
+let name = "hello"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "Hello");
+console.log(typeof null);
