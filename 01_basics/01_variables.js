5c31ec4e2f5b7e48e659c758fbaf9a8797346f39
SanDhya <141916657+sandhya324@users.noreply.github.com>
2026-06-02 20:37:07 +0530

    learning js variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,15 @@
-const accoutId = 1444334 
+const accountId = 1444334 
 let accountEmail = "sdhya489@gmail.com"
 var accountPassword = "15635"
-acccountCity = "Jaipur"
+accountCity = "Jaipur"
 
-accountId = 2
+// accountId = 2  //not allowed to change const 
+
+accoutEmail = "sadh24@gmail.com"
+accountPassword = "74674"
+accountCity = "Bengaluru"
 
 console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// { } curly braces are called scopes 
