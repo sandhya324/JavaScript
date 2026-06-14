25f048f0fdbbd4ec8cdb8b6811ad91f443bd36c2
SanDhya <141916657+sandhya324@users.noreply.github.com>
2026-06-14 13:29:15 +0000

    lecture 13

---
+++ 01_basics/07_datasinJs.js
@@ -0,0 +1,35 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); //object
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myTimeStamp.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleDateString('default' , {
+    weekday: "long",
+})
+
